6798579de97cec00ffb54c6662151e06c5846605
Thiago Santana <47760342+thiagorsdev@users.noreply.github.com>
2019-11-26 20:37:59 -0300

    Adding my analytics code (#4)

--- gatsby-config.js
+++ gatsby-config.js
@@ -68,7 +68,7 @@ module.exports = {
 		{
 			resolve: `gatsby-plugin-google-analytics`,
 			options: {
-				trackingId: "UA-XXXX-Y",
+				trackingId: "UA-147978904-1",
 				// Puts tracking script in the head instead of the body
 				head: true,
 				// IP anonymization for GDPR compliance
