87f4f6f8eee64b7942135dfaccb8dba76cfeeded
Thiago Santana <47760342+thiagorsdev@users.noreply.github.com>
2019-12-08 16:17:45 -0300

    footer fix (#14)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,104 +1,118 @@
-import { Link } from 'gatsby';
-import { setLightness } from 'polished';
-import * as React from 'react';
-import styled from '@emotion/styled';
-import { css } from '@emotion/core';
+import { Link } from "gatsby";
+import { setLightness } from "polished";
+import * as React from "react";
+import styled from "@emotion/styled";
+import { css } from "@emotion/core";
 
-import { colors } from '../styles/colors';
-import { outer, inner } from '../styles/shared';
-import config from '../website-config';
+import { colors } from "../styles/colors";
+import { outer, inner } from "../styles/shared";
+import config from "../website-config";
 
 const SiteFooter = css`
-  position: relative;
-  padding-top: 20px;
-  padding-bottom: 60px;
-  color: #fff;
-  background: ${setLightness('0.0015', colors.darkgrey)};
+	position: relative;
+	padding-top: 20px;
+	padding-bottom: 60px;
+	color: #fff;
+	background: ${setLightness("0.0015", colors.darkgrey)};
 `;
 
 const SiteFooterContent = css`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
-  color: rgba(255, 255, 255, 0.7);
-  font-size: 1.3rem;
-  a {
-    color: rgba(255, 255, 255, 0.7);
-  }
-  a:hover {
-    color: rgba(255, 255, 255, 1);
-    text-decoration: none;
-  }
-  @media (max-width: 650px) {
-    flex-direction: column;
-  }
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-between;
+	align-items: center;
+	color: rgba(255, 255, 255, 0.7);
+	font-size: 1.3rem;
+	a {
+		color: rgba(255, 255, 255, 0.7);
+	}
+	a:hover {
+		color: rgba(255, 255, 255, 1);
+		text-decoration: none;
+	}
+	@media (max-width: 650px) {
+		flex-direction: column;
+	}
 `;
 
 const SiteFooterNav = styled.nav`
-  display: flex;
+	display: flex;
 
-  a {
-    position: relative;
-    margin-left: 20px;
-  }
+	a {
+		position: relative;
+		margin-left: 20px;
+	}
 
-  a:before {
-    content: '';
-    position: absolute;
-    top: 11px;
-    left: -11px;
-    display: block;
-    width: 2px;
-    height: 2px;
-    background: #fff;
-    border-radius: 100%;
-  }
+	a:before {
+		content: "";
+		position: absolute;
+		top: 11px;
+		left: -11px;
+		display: block;
+		width: 2px;
+		height: 2px;
+		background: #fff;
+		border-radius: 100%;
+	}
 
-  a:first-of-type:before {
-    display: none;
-  }
-  @media (max-width: 650px) {
-    a:first-child {
-      margin-left: 0;
-    }
-  }
+	a:first-of-type:before {
+		display: none;
+	}
+	@media (max-width: 650px) {
+		a:first-child {
+			margin-left: 0;
+		}
+	}
 `;
 
 const Footer: React.FC = () => {
-  return (
-    <footer css={[outer, SiteFooter]}>
-      <div css={[inner, SiteFooterContent]}>
-        <section className="copyright">
-          <Link to="/">{config.title}</Link> &copy; {new Date().getFullYear()}{' '}
-          {config.footer && (
-            <Link to="/">
-              | {config.title} {config.footer}
-            </Link>
-          )}
-        </section>
-        <SiteFooterNav>
-          <Link to="/">Latest Posts</Link>
-          {config.facebook && (
-            <a href={config.facebook} target="_blank" rel="noopener noreferrer">
-              Facebook
-            </a>
-          )}
-          {config.twitter && (
-            <a href={config.twitter} target="_blank" rel="noopener noreferrer">
-              Twitter
-            </a>
-          )}
+	return (
+		<footer css={[outer, SiteFooter]}>
+			<div css={[inner, SiteFooterContent]}>
+				<section className="copyright">
+					<Link to="/">{config.title}</Link> &copy;{" "}
+					{new Date().getFullYear()}{" "}
+					{config.footer && (
+						<Link to="/">
+							| {config.title} {config.footer}
+						</Link>
+					)}
+				</section>
+				<SiteFooterNav>
+					<Link to="/">Latest Posts</Link>
+					{config.linkedin && (
+						<a
+							href={config.linkedin}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Linkedin
+						</a>
+					)}
+					{config.twitter && (
+						<a
+							href={config.twitter}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Twitter
+						</a>
+					)}
+					{config.instagram && (
+						<a
+							href={config.instagram}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Instagram
+						</a>
+					)}
 
-          <a href="https://ghost.org" target="_blank" rel="noopener noreferrer">
-            Ghost
-          </a>
-
-          <a href="/rss.xml">RSS</a>
-        </SiteFooterNav>
-      </div>
-    </footer>
-  );
+					<a href="/rss.xml">RSS</a>
+				</SiteFooterNav>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
